6e726c07eab6646f02910bbb439eb56cc23ca479
Michaelwucoc <goodluckmichael0@163.com>
2022-08-18 17:33:06 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,21 @@ import pygame
 
 class AlienInvasion:
 	"""page 205"""
-	def _init_(self)
-	pygame.init.()
+	def _init_(self):
+		pygame.init()
+
+		self.screen=pygame.display.set_mode((1200,800))
+		pygame.display.set_caption("Alien Invasion")
+	def run_game(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type==pygame.QUIT:
+					sys.exit()
+
+			pygame.display.flip()
+
+if _name_ == "_main_":
+	ai=AlienInvasion()
+	ai.run_game
 
 	self.screen=pygame.display.set_mode((1200,800))

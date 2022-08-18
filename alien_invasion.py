d7b58de2ff2d182c8432de4d895472131354ac18
Michaelwucoc <goodluckmichael0@163.com>
2022-08-18 18:49:48 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,16 @@
 import sys
 
 import pygame
-
+from settings import Settings
 class AlienInvasion:
 	"""page 205"""
 	def __init__(self):
 		#初始化资源
 		pygame.init()
+		self.settings=Settings()
+
+		self.screen=pygame.display.set_mode(
+			(self.settings.screen_width,self.settings.screen_height))
 
 		self.screen=pygame.display.set_mode((1200,800))
 		pygame.display.set_caption("Alien Invasion")
@@ -17,8 +21,7 @@ class AlienInvasion:
 				if event.type==pygame.QUIT:
 					sys.exit()
 			#每次退出循环绘制屏幕
-					self.screen.fill(self.bg_color)
-			#让绘制屏幕可见
+
 			pygame.display.flip()
 
 if __name__ == '__main__':
